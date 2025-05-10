7ed3f164fb56f71fc2cb0f9fc9ba0149fe778b9d
urmoit <89414990+urmoit@users.noreply.github.com>
2025-05-11 00:57:57 +0300

    Create latest.js

---
+++ latest.js
@@ -0,0 +1,6 @@
+{
+  "title": "Pet Simulator 99 - Fantasy World!",
+  "summary": "🌎 Fantasy World is LIVE! ⏰ Race in Time Trials to win the Rainbow Shiny Gargantuan! 🎣 And Fishing EXPANSION! 🍀 Fish in LUCK whirlpools at Blizzard Bay!",
+  "icon": "https://i.imgur.com/I0vEIqg.png",  // URL of the post's icon or image
+  "blogLink": "https://www.biggames.io/post/pet-simulator-99-update-59x"     // Link to the full blog post
+}
